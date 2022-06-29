5e022f6bc479ff98b6ac47d00f314b855fd80fec
doongjohn <doongjohn@gmail.com>
2022-06-30 02:10:23 +0900

    update

---
+++ Assets/Dev/Monster/MonsterAnim.cs
@@ -0,0 +1,7 @@
+public static class MonsterAnim
+{
+    public const string Stand = "Stand";
+    public const string Walk = "Walk";
+    public const string StrafeLeft = "Walk Strafe Left";
+    public const string StrafeRight = "Walk Strafe Right";
+}
--- Assets/Dev/Monster/MonsterFsm.cs
+++ Assets/Dev/Monster/MonsterFsm.cs
@@ -50,14 +50,6 @@ TODO:
     - [ ] 순간이동
 */
 
-public static class MonsterAnim
-{
-    public const string Stand = "Stand";
-    public const string Walk = "Walk";
-    public const string StrafeLeft = "Walk Strafe Left";
-    public const string StrafeRight = "Walk Strafe Right";
-}
-
 [System.Serializable]
 public class MonsterData
 {
